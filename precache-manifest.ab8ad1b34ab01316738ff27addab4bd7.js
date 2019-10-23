27ce12349dac7f96a6b3f581ddeac8ffc0ab09aa
Challe <gabin.challe@outlook.fr>
2019-10-23 09:23:25 +0200

    initial commit

---
+++ precache-manifest.ab8ad1b34ab01316738ff27addab4bd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac09c1a377ef9807c7e",
+    "url": "/css/app.ba629074.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b2788a6579be7ea0f9d6560e1d6397d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a5831f4e54ff48bc0d",
+    "url": "/js/about.e97b8d0b.js"
+  },
+  {
+    "revision": "1ac09c1a377ef9807c7e",
+    "url": "/js/app.f40f4661.js"
+  },
+  {
+    "revision": "18a8dff35c2ee226a844",
+    "url": "/js/chunk-vendors.d47cf30f.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
